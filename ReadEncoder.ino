35d5eab2c6c090c36cae358a302646512d3c0424
Stefan Helmert <stefan.helmert@t-online.de>
2024-10-30 14:51:31 +0100

    śample all encoders in timer interrupt

--- ReadEncoder.ino
+++ ReadEncoder.ino
@@ -5,13 +5,19 @@
 /* The maximal digital value */
 #define MAX_DIGITAL     (4095)
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
 int led = 13;
 
 class RotaryEncoder {
   public:
     RotaryEncoder(void);
-    void setIpin(int pin);
-    void setQpin(int pin);
+    //void setIpin(int pin);
+    //void setQpin(int pin);
+    void setIcb(int (*iSrcFn) (void) );
+    void setQcb(int (*qSrcFn) (void) );
     void sample(void);
     int getValue(void);
     void reset(int value);
@@ -19,8 +25,10 @@ class RotaryEncoder {
   private:
     int cnt;
     int state;
-    int iPin;
-    int qPin;
+    //int iPin;
+    //int qPin;
+    int (*iSrcFn) (void);
+    int (*qSrcFn) (void);
 
 };
 
@@ -28,14 +36,22 @@ RotaryEncoder::RotaryEncoder(void) {
 
 }
 
+void RotaryEncoder::setIcb(int (*iSrcFn) (void) ) {
+  this->iSrcFn = iSrcFn;
+}
+
+void RotaryEncoder::setQcb(int (*qSrcFn) (void) ) {
+  this->qSrcFn = qSrcFn;
+}
+/*
 void RotaryEncoder::setIpin(int pin) {
-  this->iPin = pin;
+  this->iSrcFn = pin;
 }
 
 void RotaryEncoder::setQpin(int pin) {
   this->qPin = pin;
 }
-
+*/
 int RotaryEncoder::getValue(void) {
   return this->cnt;
 }
@@ -46,8 +62,10 @@ void RotaryEncoder::reset(int value=0) {
 
 void RotaryEncoder::sample(void) {
   const int lut[4] = {0, 1, 3, 2};
-  int i = digitalRead(this->iPin);
-  int q = digitalRead(this->qPin);
+  //int i = digitalRead(this->iPin);
+  //int q = digitalRead(this->qPin);
+  int i = this->iSrcFn();
+  int q = this->qSrcFn();
   int cur = lut[(i + 2 * q)%4];
   int diff = (cur - this->state + 4)%4;
   this->state = cur;
@@ -58,12 +76,57 @@ void RotaryEncoder::sample(void) {
   }
 }
 
-RotaryEncoder enc1;
+void setup_timer(void){
+  // Timer1 konfigurieren
+  cli(); // Alle Interrupts deaktivieren, um Konfiguration sicher zu machen
+
+  TCCR1A = 0;            // Timer1 Control Register A auf 0 setzen
+  TCCR1B = 0;            // Timer1 Control Register B auf 0 setzen
+  TCNT1  = 0;            // Timerzähler auf 0 setzen
+
+  // Vergleichswert berechnen für 1ms Interrupts:
+  // Bei einem 16MHz Arduino-Takt und einem Prescaler von 64
+  // ist 1ms = 16,000,000 / 64 / 1000 = 250 Zählschritte.
+  OCR1A = 250;           // Vergleichswert setzen für 1ms
+
+  TCCR1B |= (1 << WGM12); // CTC Modus (Clear Timer on Compare Match)
+  TCCR1B |= (1 << CS11) | (1 << CS10); // Prescaler auf 64 setzen
+
+  TIMSK1 |= (1 << OCIE1A); // Timer Compare Interrupt aktivieren
+
+  sei(); // Interrupts global aktivieren
+}
+
+
+RotaryEncoder rotenc[5];
+
+void setup_all_encoders(void) {
+  for (int i=0; i<5; i++) {
+    rotenc[i].setIcb([](int i) {return digitalRead(2*i+4);});
+    rotenc[i].setQcb([](int i) {return digitalRead(2*i+5);});
+    rotenc[i].reset();
+  }
+
+}
+
+void sample_all_encoders(void) {
+  for (int i=0; i<5; i++) {
+    rotenc[i].sample();
+  }
+}
+
+ISR(TIMER1_COMPA_vect) {
+  // Diese Funktion wird alle 1 ms aufgerufen
+  // Hier kann Code eingefügt werden, der alle 1ms ausgeführt werden soll.
+  sample_all_encoders();
+}
+
 
 void setup() {
-  enc1.setIpin(4);
-  enc1.setQpin(5);
-  enc1.reset();
+  setup_all_encoders();
+  setup_timer();
+//  enc1.setIpin(4);
+//  enc1.setQpin(5);
   Serial.begin(115200);
   //pinMode(led, OUTPUT);
   for(int i=4;i<14;i++){
@@ -81,14 +144,17 @@ void setup() {
 void loop() {
   //int i_enc = digitalRead(51);
   //int i_enc = PIO_PD1;
-  enc1.sample();
+  //enc1.sample();
   Serial.print("Digital:");
   for(int i=4;i<14;i++){
     Serial.print(" ");
     Serial.print(digitalRead(i));
   }
-  Serial.print(" # ");
-  Serial.print(enc1.getValue());
+  Serial.print(" #");
+  for (int i=0; i<5; i++) {
+    Serial.print(" ");
+    Serial.print(rotenc[i].getValue());
+  }
   Serial.println("");
 
 
